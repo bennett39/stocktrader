f4fca7af0f5deb79a5b3cdb024ac29cb16dee23e
Bennett <34491412+bennett39@users.noreply.github.com>
2019-08-25 13:47:12 -0400

    Add STATIC_ROOT and remove boilerplate comments

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,4 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
